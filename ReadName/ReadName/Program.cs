616e0fbae0f9f5f68b8b7006b9e0dcbbe70f6b57
ItsSony2003 <sonythaiduong2003@gmail.com>
2024-11-19 21:17:04 +0700

    Create Program.cs

---
+++ ReadName/ReadName/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadName
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.Write("Enter your name: ");
+
+            string yourName = Console.ReadLine();
+
+            Console.WriteLine("Hello: " + yourName);
+
+            Console.ReadKey();
+        }
+    }
+}
