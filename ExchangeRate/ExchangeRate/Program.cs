fd55cbfd6ce0243b952854cf2f3ccc9599a601c2
ItsSony2003 <sonythaiduong2003@gmail.com>
2024-11-19 21:14:37 +0700

    Create Program.cs

---
+++ ExchangeRate/ExchangeRate/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExchangeRate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float usd;
+            Console.Write("Input USD: ");
+            usd = float.Parse(Console.ReadLine());
+
+            float rate = usd * 23000;
+            Console.WriteLine("VND is: " + rate);
+            Console.ReadKey();
+        }
+    }
+}
