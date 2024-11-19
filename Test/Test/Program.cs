d5d2dd8a7720f44a71090fe226ba0af248368411
ItsSony2003 <sonythaiduong2003@gmail.com>
2024-11-19 19:55:59 +0700

    Create Program.cs

---
+++ Test/Test/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!!");
+            Console.WriteLine();
+
+            DateTime localDate = DateTime.Now;
+            System.Console.WriteLine("Datetime Now is :" + localDate);
+            Console.ReadKey();
+        }
+    }
+}
